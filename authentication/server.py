29615ea2c5c3ef3fc8d57beca4adf6e471c99eb3
lfuksman <61123718+lfuksman@users.noreply.github.com>
2020-03-19 01:01:49 -0500

    Add files via upload

---
+++ authentication/server.py
@@ -0,0 +1,91 @@
+from functools import wraps
+import json
+from os import environ as env
+from werkzeug.exceptions import HTTPException
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, jsonify, redirect, render_template, session, url_for
+from authlib.integrations.flask_client import OAuth
+from six.moves.urllib.parse import urlencode
+
+app = Flask(__name__)
+AUTH0_AUDIENCE = 'https://dev-92zb026a.auth0.com/userinfo'
+app.secret_key = 'secret_key'
+JWT_PAYLOAD = 'jwt_payload'
+PROFILE_KEY = 'profile'
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id='90ycfYTmNMgEpY7x4a2fquPoW5iB0N77',
+    client_secret='RE-7Yz3KMufnrtZIQIHNYzPTfp8V_U2CSHuwkqK7mDinYZ5LW4CcCuQkCP64GvF6',
+    api_base_url='https://dev-92zb026a.auth0.com',
+    access_token_url='https://dev-92zb026a.auth0.com/oauth/token',
+    authorize_url='https://dev-92zb026a.auth0.com/authorize',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+)
+
+
+@app.route('/')
+def home():
+    return render_template('homeScreen.html')
+
+
+# Here we're using the /callback route.
+@app.route('/callback')
+def callback_handling():
+    # Handles response from token endpoint
+    print "redirected here"
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/dashboard')
+
+
+@app.route('/login')
+def login():
+    print "Login"
+    #return auth0.authorize_redirect(redirect_uri='http://localhost:3000/callback', audience=AUTH0_AUDIENCE)
+    return auth0.authorize_redirect(redirect_uri='http://localhost:3000/callback')
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'profile' not in session:
+            # Redirect to Login page here
+            return redirect('/login')
+        return f(*args, **kwargs)
+    return decorated
+
+
+@app.route('/dashboard')
+@requires_auth
+def dashboard():
+    return render_template('dashboard.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
+
+
+@app.route('/logout')
+def logout():
+    # Clear session stored data
+    session.clear()
+    # Redirect user to logout endpoint
+    params = {'returnTo': url_for('home', _external=True), 'client_id': '90ycfYTmNMgEpY7x4a2fquPoW5iB0N77'}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=env.get('PORT', 5000))
+
